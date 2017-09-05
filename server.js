9966873d1e33aa2ea8fced67ca4c32b4d5d047d6
Ravinder Bhatia <ravinderb@icloud.com>
2017-09-05 12:35:52 -0700

    Adding new component users in the previous commit and setting up user routes in express

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ var http = require('http');
 const app = express();
 
 const api = require('./server/routes/api');
+app.use('/api', api);
+
+const users = require('./server/routes/users');
+app.use('/users', users);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'client/dist')));
-
-app.use('/api', api);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/dist/index.html'));
